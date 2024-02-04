d508c60a299fd6c93b5259d20e8eb3f9a63a700f
galynary <107271972+galynary@users.noreply.github.com>
2024-02-04 19:31:50 +0200

    update server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const { DB_HOST, PORT = 3000 } = process.env;
+const { DB_HOST, PORT = 2000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
@@ -10,7 +10,7 @@ mongoose
 	.connect(DB_HOST)
 	.then(() => {
 		app.listen(PORT, () => {
-			console.log("Server running. Use our API on port: 3000");
+			console.log("Server running. Use our API on port:2000");
 		});
 	})
 	.catch((error) => {
